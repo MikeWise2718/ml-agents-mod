7e075785729f7a060be8db6204b3b074821fd078
MikeWise2718 <mwise@microsoft.com>
2019-11-16 18:01:28 +0100

    Signed-off-by: MikeWise2718 <mwise@microsoft.com>

--- UnitySDK/Assets/ML-Agents/Scripts/Grpc/RpcCommunicator.cs
+++ UnitySDK/Assets/ML-Agents/Scripts/Grpc/RpcCommunicator.cs
@@ -332,17 +332,18 @@ namespace MLAgents
             return m_LastActionsReceived[key];
         }
 
-        string messRootDirName = "messages";
+        public static bool doMessLogging = false;
+        public static string messRootDirName = "messages";
+        public static int maxLogStep = 100;
+        public static int freqModulo = 1;
         string messDirName = "";
         string messSubDirName = "";
         int stepNum = 0;
 
-        int maxLogStep = 100;
         string fmt = "D3";
-        public void SaveMessageText(string filename, string sdata,bool incstep=false)
+        public void DoSaveMessageText(string filename, string sdata)
         {
-            if (stepNum > maxLogStep) return;
-            if (messSubDirName=="")
+            if (messSubDirName == "")
             {
                 messSubDirName = DateTime.Now.ToString("yyyyMMdd-HHmmss");
                 messDirName = messRootDirName + "/" + messSubDirName;
@@ -354,9 +355,23 @@ namespace MLAgents
                 System.IO.Directory.CreateDirectory(messDirName);
             }
             var sstep = stepNum.ToString(fmt);
-            var fname = messDirName + "/" + filename + sstep+ ".json";
+            var fname = messDirName + "/" + filename + sstep + ".json";
             Debug.Log("Writing " + fname + " len:" + sdata.Length);
             System.IO.File.WriteAllText(fname, sdata);
+        }
+
+        public void SaveMessageText(string filename, string sdata,bool incstep=false)
+        {
+            if (doMessLogging)
+            {
+                if (stepNum <= maxLogStep)
+                {
+                    if (freqModulo <= 1 || (stepNum % freqModulo == 0))
+                    {
+                        DoSaveMessageText(filename, sdata);
+                    }
+                }
+            }
             if (incstep)
             {
                 stepNum++;
